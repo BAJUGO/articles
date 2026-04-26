cd277b6efa4b3ddcb78698978ba1fe4c88257a85
miloradik <texetbuy@gmail.com>
2026-04-26 23:12:28 +0300

    Prometheus via Histogram

--- backend/back_backend/middleware.py
+++ backend/back_backend/middleware.py
@@ -1,18 +1,30 @@
-from prometheus_client import Counter
+import time
+from prometheus_client import Histogram
+from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi import Request
 
-from fastapi import Request, BackgroundTasks
-
-REQUEST_COUNT = Counter(
-    "http_request_count", "TOTAL NUMBER OF REQUESTS",
-    ["app_name", "method", "path", "http_status"]
+HTTP_REQUEST_DURATION = Histogram(
+    "http_request_duration_seconds",
+    "Duration of HTTP request in seconds",
+    labelnames=["method", "path", "status"],
+    buckets=[0.01, 0.05, 0.1, 0.25, 0.5, 1.0, 2.0, 5.0, 10.0]
 )
 
-async def my_middleware(request: Request, call_next):
-    response = await call_next(request)
-    REQUEST_COUNT.labels(
-                        app_name="backend",
-                        method=request.method,
-                        path=request.url.path,
-                        http_status=str(response.status_code)
-                        ).inc()
-    return response
+class PrometheusMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        route = request.scope.get("route")
+        path = route.path if route else request.url.path
+
+        start_time = time.perf_counter()
+
+        response = await call_next(request)
+
+        duration = time.perf_counter() - start_time
+
+        HTTP_REQUEST_DURATION.labels(
+            method=request.method,
+            path=path,
+            status=response.status_code
+        ).observe(duration)
+
+        return response
