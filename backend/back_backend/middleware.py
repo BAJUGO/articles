f615aeac5161a6262682951371239d35e8278deb
miloradik <texetbuy@gmail.com>
2026-04-27 16:55:25 +0300

    grafana barely working

--- backend/back_backend/middleware.py
+++ backend/back_backend/middleware.py
@@ -7,17 +7,19 @@ HTTP_REQUEST_DURATION = Histogram(
     "http_request_duration_seconds",
     "Duration of HTTP request in seconds",
     labelnames=["method", "path", "status"],
-    buckets=[0.01, 0.05, 0.1, 0.25, 0.5, 1.0, 2.0, 5.0, 10.0]
+    buckets=[0.001, 0.01, 0.05, 0.1, 0.25, 0.5, 1.0, 2.0, 5.0, 10.0]
 )
 
 class PrometheusMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        route = request.scope.get("route")
-        path = route.path if route else request.url.path
-
         start_time = time.perf_counter()
 
+
         response = await call_next(request)
+
+        route = request.scope.get("route")
+        path = route.path_format if route else "not_found"
+
 
         duration = time.perf_counter() - start_time
 
